2c082154f18b513a5dfce841afdcd0b2fd99136a
cheesecake-69 <karora_be21@thapar.edu>
2022-10-10 00:14:26 +0530

    Added first python code

---
+++ pythonCode/fibonacci number.py
@@ -0,0 +1,10 @@
+def fibonacci_number(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    else:
+        return fibonacci_number(n-1) + fibonacci_number(n-2)
+
+print(fibonacci_number(int(input())))
+
