3c57ba1a1a659962f9fa77886bccd571d56e9c9c
Utsavrai <Utsavrai890@gmail.com>
2022-10-10 00:56:57 +0530

    Added Pattern Programs in C

---
+++ Pattern Programs in C/Floyd triangle.c
@@ -0,0 +1,19 @@
+#include <stdio.h>  
+#include <conio.h>  
+void main()  
+{   
+    int i, j, rows, k = 1;  
+    printf (" Enter a number to define the rows: \n ");  
+    scanf("%d", &rows);   
+    printf("\n");  
+    for (i = 1; i <= rows; i++)   
+    {  
+        for (j = 1; j <= i; j++)   
+        {  
+            printf("%d ", k);   
+            k += 1;  
+        }  
+        printf("\n");  
+    }  
+    getch();      
+}  
