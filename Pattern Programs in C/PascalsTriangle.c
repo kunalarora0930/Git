83a13bfd1d6f7069f7673d103c3d44b7dd3d2d91
segFault69 <115822749+segFault69@users.noreply.github.com>
2022-10-14 23:38:03 +0530

    Update PascalsTriangle.c

--- Pattern Programs in C/PascalsTriangle.c
+++ Pattern Programs in C/PascalsTriangle.c
@@ -1,23 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
 long factorial(int n){
     if(n==0||n==1){
         return 1;
     }
     return n*factorial(n-1);
 }
-int ncr(int n,int r){
+int ncr(int n ,int r){
     return factorial(n)/(factorial(n-r)*factorial(r));
 }
-int main(){
-    int n,i,j,k;
+int main() {
+    int n, i, j, k;
     printf("\n Enter the number of rows:");
-    scanf("%d",&n);
-    for(i=0;i<n;i++){
-        for(k=n-1-i;k>=1;k--){
+    scanf("%d", &n);
+    for(i=0; i<n; i++){
+        for(k = n - 1 - i; k >= 1; k--) {
             printf("  ");
         }
-        for(j=0;j<=i;j++){
-            printf("%d   ",ncr(i,j));
+        for(j = 0; j <= i; j++) {
+            printf("%d   ", ncr(i, j));
         }
         printf("\n");
 
