03b87fe409c965293421537b5df530cfc0227152
CHITRA DHIR <91721427+CHITRADHIR@users.noreply.github.com>
2022-10-23 19:34:10 +0530

    Update quicksort.cpp

--- CHITRADHIR/quicksort.cpp
+++ CHITRADHIR/quicksort.cpp
@@ -9,21 +9,25 @@ int main(){
   
     int *input = new int[n];
     
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++)
+    {
         cin >>    input[i];
     }
     
     quickSort(input, n);
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++) 
+    {
         cout << input[i] << " ";
     }
     
     delete [] input;
+    cout<<"worst case time complexity of Quick sort is O(n^2)"<<endl;
 
 }
 
 
-void quickSort(int input[], int size) {
+void quickSort(int input[], int size)
+{
 
     if(size==0||size==1)
         return;
@@ -50,6 +54,7 @@ void quickSort(int input[], int size) {
         i++;
         j--;
         }
+        
     }
     quickSort(input,counter);
     quickSort(input+counter+1,size-1-counter);
