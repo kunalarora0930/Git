c5a849ff96cf1c42d9e99cabea096ed1468a030a
RattanAshishSimon <115374217+RattanAshishSimon@users.noreply.github.com>
2022-10-24 11:02:34 +0530

    Update print.cpp

--- CHITRADHIR/print.cpp
+++ CHITRADHIR/print.cpp
@@ -1,8 +1,17 @@
 #include<iostream>
 using namespace std;
+int length(Node *head) {
+    // Write your code here
+
+    if (head == NULL)
+        return 0;
+    return 1 + length(head->next);
+  
+}
+
 int main()
 {
-
+cout<<"length to tshirt ke liye daali hai"<<endl;
 
     cout<<"HI";
     cout<<"LIFE IS A RACE"<<endl;
